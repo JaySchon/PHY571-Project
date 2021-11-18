37f700f5a97c51a41296df131e76c2546e91ea9d
Jie Xiong <93990829+JaySchon@users.noreply.github.com>
2021-11-18 08:28:05 +0800

    Update LocalUpdate.py
    Change the way of updating. Now we only update the spins matrix instead of update the whole configuration.

--- LocalUpdate.py
+++ LocalUpdate.py
@@ -4,29 +4,29 @@
 import numpy.random as rnd
 import numpy as np
 
-class LocalUpdator():
+class LocalUpdate():
     
-    def __init__(self, config):
-        self.config = config
-        L = config.size
-        self.J = config.J
-        
+    def __init__(self, spins, J, K, T):
+        self.spins = spins
+        self.L = len(spins)
+        self.J = J
+        self.K = K
+        self.beta = 1./ T
+         
     def local_update(self):
         i, j = rnd.randint(L, size=(2)) # pick a random site
     
         # compute energy difference
-        coef = 2 * config.spins[i,j]
-        delta_energy = J * coef * (self.config.spins[i,(j+1)%self.L] + self.config.spins[(i+1)%self.L,j] + self.config.spins[i,(j-1)%self.L] + self.config.spins[(i-1)%self.L,j]) +\
-                        K * coef * (self.config.spins[i,(j+1)%self.L] * self.config.spins[(i+1)%self.L,(j+1)%self.L] * self.config.spins[(i+1)%self.L,j] + \
-                                    self.config.spins[i,(j+1)%self.L] * self.config.spins[(i-1)%self.L,(j+1)%self.L] * self.config.spins[(i-1)%self.L,j] + \
-                                    self.config.spins[i,(j-1)%self.L] * self.config.spins[(i-1)%self.L,(j-1)%self.L] * self.config.spins[(i-1)%self.L,j] + \
-                                    self.config.spins[i,(j-1)%self.L] * self.config.spins[(i+1)%self.L,(j-1)%self.L] * self.config.spins[(i+1)%self.L,j])
+        coef = 2 * spins[i,j]
+        delta_energy = J * coef * (self.spins[i,(j+1)%self.L] + self.spins[(i+1)%self.L,j] + self.spins[i,(j-1)%self.L] + self.spins[(i-1)%self.L,j])\
+                  + K * coef * (self.spins[i,(j+1)%self.L] * self.spins[(i+1)%self.L,(j+1)%self.L] * self.spins[(i+1)%self.L,j] + \
+                           self.spins[i,(j+1)%self.L] * self.spins[(i-1)%self.L,(j+1)%self.L] * self.spins[(i-1)%self.L,j] + \
+                           self.spins[i,(j-1)%self.L] * self.spins[(i-1)%self.L,(j-1)%self.L] * self.spins[(i-1)%self.L,j] + \
+                           self.spins[i,(j-1)%self.L] * self.spins[(i+1)%self.L,(j-1)%self.L] * self.spins[(i+1)%self.L,j])
 
         # accept modification with Metropolis probability
         # if not accepted: leave configuration unchanged
-        if rnd.random() < np.exp(-self.config.beta * delta_energy):
-            self.config.spins[i,j] *= -1
-            self.config.energy += delta_energy
-            self.config.magnetization += 2*self.config.spins[i,j]
+        if rnd.random() < np.exp(- self.beta * delta_energy):
+            self.spins[i,j] *= -1
             
-        return config
+        return self.spins
