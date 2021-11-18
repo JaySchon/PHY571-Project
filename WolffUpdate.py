70557327a95ba2d82966361f77deb5dc23624701
Jie Xiong <93990829+JaySchon@users.noreply.github.com>
2021-11-18 08:30:11 +0800

    Update WolffUpdate.py

--- WolffUpdate.py
+++ WolffUpdate.py
@@ -2,35 +2,35 @@
 ### import libraries
 import numpy as np
 import numpy.random as rnd
+from Configuration import four_body_sum as four_body_sum
 
-
-class WolffUpdator():
+class WolffUpdate():
     
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, spins, J, K, T):
+        self.spins = spins
+        self.size = len(spins)
         self.cluster = []
-        self.J = config.J
-        
+        self.J = J
+        self.K = K
+        self.T = T
+        self.beta = 1./ T
         
     def activate_prob(self, point, neigh):
-        return 1 - np.exp(- 2 * self.config.beta * self.J * \
-                                  self.config.spins[point[0],point[1]] * self.config.spins[neigh[0],neigh[1]])
+        return 1 - np.exp(- 2 * self.beta * self.J * \
+                                  self.spins[point[0],point[1]] * self.spins[neigh[0],neigh[1]])
     
     def add_site(self, point, neigh):
         ### add sites to the cluster (1st NN)
-        size = self.config.size
         
         if neigh not in self.cluster:
             prob = self.activate_prob(point, neigh)
             # check if the link is activated
             if rnd.random() < prob:
-                # self.config.spins(neigh) *= -1
-                self.cluster.append(neigh)
-            
-                self.extend_cluster(neigh)
+                self.cluster.append(neigh) # add point to the cluster            
+                self.extend_cluster(neigh) # extend the cluster from the center positioned at 'neighbour point'
             
     def extend_cluster(self, point):
-        size = self.config.size
+        size = len(self.spins)
         ## check if the neighbors should be added to the cluster, in anti-clockwise direction
         self.add_site(point, [(point[0]+1)%size, point[1]])
         self.add_site(point, [point[0], (point[1]+1)%size])
@@ -39,66 +39,54 @@ class WolffUpdator():
         
     
     def Wolff_Update_0(self):
-        ### Build the cluster and flip it with probability one (no external source of energy)
-        L = self.config.size
+        ### Build the cluster and flip it with probability one 
         
         #randomly pick a site and add to the cluster
-        i, j = rnd.randint(L, size=(2)) 
+        i, j = rnd.randint(self.size, size=(2)) 
         self.cluster.append([i,j])
         
         #check adjacent states to build the whole cluster
-        self.add_site([i,j],[(i+1) % L,j])
-        self.add_site([i,j],[i,(j+1) % L])
-        self.add_site([i,j],[(i-1) % L,j])
-        self.add_site([i,j],[i,(j-1) % L])
-        
+        self.add_site([i,j],[(i+1) % self.size,j])
+        self.add_site([i,j],[i,(j+1) % self.size])
+        self.add_site([i,j],[(i-1) % self.size,j])
+        self.add_site([i,j],[i,(j-1) % self.size])
         
         # flip all the site in the cluster with prob 1
         for site in self.cluster:
-            self.config.spins[site[0],site[1]] *= -1 
-        
-        self.config.energy = self.config._get_energy()
-        self.config.magnetization = self.config._get_magnetization()
-        
-        return self.config
+            self.spins[site[0],site[1]] *= -1 
+        return self.spins
         
         
     def Wolff_Update_1(self):
         ### build the cluster and flip it with probability relevant to external sources 
-        L = self.config.size
-        
-        energy_1_ini = self.config.get_energy_1()
-        
+       
+        energy_1_ini = -self.K * four_body_sum(self.spins)
         
         #randomly pick a site and add to the cluster
         i, j = rnd.randint(L, size=(2)) 
         self.cluster.append([i,j])
         
         #check adjacent states to build the whole cluster
-        self.add_site([i,j],[(i+1) % L,j])
-        self.add_site([i,j],[i,(j+1) % L])
-        self.add_site([i,j],[(i-1) % L,j])
-        self.add_site([i,j],[i,(j-1) % L])
+        self.add_site([i,j],[(i+1) % self.size,j])
+        self.add_site([i,j],[i,(j+1) % self.size])
+        self.add_site([i,j],[(i-1) % self.size,j])
+        self.add_site([i,j],[i,(j-1) % self.size])
         
         # flip the cluster and calclate the energy difference of the external source term
         for site in self.cluster:
-            self.config.spins[site[0],site[1]] *= -1 
+            self.spins[site[0],site[1]] *= -1 
         
         # self.config.spins = spins
-        energy_1_flip = self.config.get_energy_1()
+        energy_1_flip = -self.K * four_body_sum(self.spins)
         energy_1_diff = energy_1_flip - energy_1_ini
         
-        prob = np.min([1, np.exp(- self.config.beta * energy_1_diff)])
+        prob = np.min([1, np.exp(- self.beta * energy_1_diff)])
         
         # check if we keep the flip
         if rnd.random() < prob:
-            self.config.energy = self.config._get_energy()
-            self.config.magnetization = self.config._get_magnetization()
-            return self.config
-            
+            return self.spins
         else: 
             for site in self.cluster:
-                self.config.spins[site[0],site[1]] *= -1
-            self.config.energy = self.config._get_energy()
-            self.config.magnetization = self.config._get_magnetization()
-            return self.config
+                self.spins[site[0],site[1]] *= -1
+            return self.spins
+
